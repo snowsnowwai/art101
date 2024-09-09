0115ed1906e75e52156bec567112c49c3e992da2
sn0w <audreycwai@gmail.com>
2024-09-09 14:35:56 -0700

    p1 changes

--- p1_ex1/sketch.js
+++ p1_ex1/sketch.js
@@ -11,7 +11,7 @@ function draw() {
     for (let i = 0; i < mouseX; i += 20) {
       let r1 = random(1, 255);
       let s1 = random(1,25);
-      fill(r1, r1-100, r1-100);
+      fill(r1, r1-100, r1-100, 90);
       ellipse(i+20, y1, s1, s1);
       console.log("r1 = " + r1);
      }
@@ -21,16 +21,16 @@ function draw() {
     for (let i = 0; i < mouseX; i += 20) {
       let r2 = random(1, 255);
       let s2 = random(5,15);
-      fill(r2, r2-70, r2+50);
+      fill(r2, r2-70, r2+50, 70);
       triangle(mouseX, y1, mouseX-s2, mouseY-s2, mouseX, mouseY+s2);
       console.log("r2 = " + r2);
     }
     
      if(mouseY > 250)  { 
-      for (let i = 0; i < mouseX; i += 2) {
+      for (let i = 10; i < mouseX; i += 25) {
         let r5 = random(1, 255);
         let s5 = random(10, 20);
-        fill(r5+i, r5, r5+20);
+        fill(r5+i, r5, r5+20, 60);
         rect(i+0, y1, s5, s5);
         console.log("r5 = " + r5);
        }
@@ -40,10 +40,10 @@ function draw() {
   else  {
     let mx = mouseX;
     for (let i = 0; i < mouseX; i += 20) {
-      let r3 = random(1,255);
-      let s3 = random(1, 25);
-      fill(r3); 
-      stroke(r3);
+      let r3 = random(1, 255);
+      let s3 = random(1, 15);
+      //fill(r3, r3-100, r3+20); 
+      stroke(r3-50, r3-10, r3+40, 60);
       strokeWeight(s3);
       point(mouseX, y1);
       console.log("r3 = " + r3);
@@ -51,9 +51,9 @@ function draw() {
 
     for (let i = 0; i < mouseX; i += 20) {
       let r4 = random(1, 100);
-      let s4 = random(1,15);
-      stroke(r4, 20-r4, r4+50);
-      strokeWeight(5);
+      let s4 = random(1,5);
+      stroke(r4-50, r4-70, r4+120);
+      strokeWeight(s4);
       line(mouseX, y1, mouseX-20, y1-20);
       console.log("r4 = " + r4);
     }
